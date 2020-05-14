6fa7ef3d102ac295b6d00c850c3c543a354d1040
Arnoldovna <marina.wilhelm@icloud.com>
2020-05-14 17:52:41 +0200

    add projects yml and map

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -1,8 +1,28 @@
 var mawPrintSpan = document.getElementById('maw-print');
+var instructSpan = document.getElementById('instruct');
+var grueneSpan = document.getElementById('gruene');
+var lalaSpan = document.getElementById('lala');
+var parmaSpan = document.getElementById('parma');
+var schrotKornSpan = document.getElementById('schrot-korn');
+var bodyLoomSpan = document.getElementById('body-loom');
+
+
 
 window.onmousemove = function (e) {
     var x = e.clientX,
         y = e.clientY;
     mawPrintSpan.style.top = (y + 20) + 'px';
     mawPrintSpan.style.left = (x + 20) + 'px';
+    grueneSpan.style.top = (y + 20) + 'px';
+    grueneSpan.style.left = (x + 20) + 'px';
+    lalaSpan.style.top = (y + 20) + 'px';
+    lalaSpan.style.left = (x + 20) + 'px';
+    parmaSpan.style.top = (y + 20) + 'px';
+    parmaSpan.style.left = (x + 20) + 'px';
+    bodyLoomSpan.style.top = (y + 20) + 'px';
+    bodyLoomSpan.style.left = (x + 20) + 'px';
+    instructSpan.style.top = (y + 20) + 'px';
+    instructSpan.style.left = (x + 20) + 'px';
+    schrotKornSpan.style.top = (y + 20) + 'px';
+    schrotKornSpan.style.left = (x + 20) + 'px';
 };
