7aef06de7892de1a5d47c114b0ca090f4c5885ae
Arnoldovna <marina.wilhelm@icloud.com>
2020-07-07 17:40:50 +0200

    push to gh pages

--- assets/js/transitions.js
+++ assets/js/transitions.js
@@ -2,6 +2,6 @@ var lines = document.getElementsByClassName('hook');
 
 const timeline = [];
 [...lines].forEach((line, i) => {
-    var s = 200 + (i * 50);
+    var s = 100 + (i * 50);
     timeline[i] = line.setAttribute("data-aos-delay", s);
 });
